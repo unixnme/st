b7e1595d26528d54c8a321365ac55bebbca0a970
Unix&Me <unixnme@gmail.com>
2021-11-29 19:49:17 -0800

    fix help; ignore empty lines

--- st.cc
+++ st.cc
@@ -4,7 +4,7 @@
 #include <cmath>
 
 int Usage(const std::string& program) {
-    std::cerr << "Calculates min, max, min, stddev, and count\n";
+    std::cerr << "Calculates count, min, max, mean, and stddev of numbers separated by newline\n";
     std::cerr << "Usage: " << program << " [file]" << "\n";
     return EXIT_FAILURE;
 }
@@ -14,7 +14,7 @@ int main(int argc, const char** argv) {
     if (argc != 1 && argc != 2) return Usage(argv[0]);
     std::string filename{argc == 1 || std::strcmp(argv[1], "-") == 0 ? "/dev/stdin" : argv[1]};
     if (filename == "--help" || filename == "-h") {
-        Usage(argv[1]);
+        Usage(argv[0]);
         return 0;
     }
 
@@ -32,7 +32,15 @@ int main(int argc, const char** argv) {
 
 
     std::string line;
+    uint64_t line_idx = 0;
     while(std::getline(ifs, line)) {
+        ++line_idx;
+        line.erase(std::remove_if(line.begin(), line.end(),
+                                  [](char x) { return std::isspace(x); }), line.end());
+        if (line.empty()) {
+            std::cerr << "warning: skipping empty line at " << line_idx << "\n";
+            continue;
+        }
         double x = std::stod(line);
         s += x; ss += x*x;
         min = std::min(min, x);
@@ -40,15 +48,19 @@ int main(int argc, const char** argv) {
         ++count;
     }
 
-    std::cout << "count\t" << count << "\n";
-    if (count != 0) {
+    do {
+        std::cout << "count\t" << count << "\n";
+        if (count == 0) break;
+
         auto count_double = static_cast<double>(count);
         std::cout << "min\t" << min << "\n";
+        std::cout << "max\t" << max << "\n";
         const auto mean = s / count_double;
         std::cout << "mean\t" << mean << "\n";
-        std::cout << "stddev\t" << sqrt((ss / count_double - mean*mean) * count_double / (count_double - 1)) << "\n";
-        std::cout << "max\t" << max << "\n";
-    }
+        if (count == 1) break;
+
+        std::cout << "stddev\t" << sqrt((ss / count_double - mean * mean) * count_double / (count_double - 1)) << "\n";
+    } while (false);
 
     return 0;
 }
